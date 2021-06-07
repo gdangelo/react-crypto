0bc635a5fbe0c8dd8e6683d63e66bdae01fb7d17
Grégory D'Angelo <dangelo.gregory@gmail.com>
2021-06-07 14:39:56 +0200

    feat: create basic page routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { Home, NotFound } from 'pages';
+
 function App() {
   return (
-    <div>
-      <header>
-        <p>Crypto App</p>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* No match - 404 */}
+        <Route exact path="/404" component={NotFound} />
+        <Redirect to="/404" />
+      </Switch>
+    </Router>
   );
 }
 
